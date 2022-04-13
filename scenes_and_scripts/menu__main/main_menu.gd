f05c0c52eb245043c315c9e6b414f1a5cc5c25df
fernandobarahona <33161338+fernandobarahona@users.noreply.github.com>
2022-04-13 10:19:47 -0500

    little visual changes New Ghub repo

--- scenes_and_scripts/menu__main/main_menu.gd
+++ scenes_and_scripts/menu__main/main_menu.gd
@@ -8,6 +8,9 @@ func _ready():
 	var _err1 = get_node("VBoxContainer/GoToGameBtn").connect("pressed",self,"goToScene",[gameScene])
 	var _err2 = get_node("VBoxContainer/GoToSettingsBtn").connect("pressed",self,"goToScene",[configMenuScene])
 	var _err3 = get_node("VBoxContainer/GoToLoginBtn").connect("pressed",self,"goToScene",[loginScene])
-	
+
 func goToScene(scene):
 	SceneManager.gotoScene(scene)
+
+func func2():
+	pass
