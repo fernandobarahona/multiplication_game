7710fc4ad9b1fae31c767d7d0a2f7e4b8c453b3d
zur11 <cibka11@gmail.com>
2022-04-19 18:10:57 -0500

    Added _fade_out() in SceneManager, works only returning to main_menu

--- scenes_and_scripts/gl_singl__scene_manager/scene_manager.gd
+++ scenes_and_scripts/gl_singl__scene_manager/scene_manager.gd
@@ -15,6 +15,7 @@ func goto_scene(sceneToGo: PackedScene, mainContainer:Node = get_node("/root/Mai
 	if mainContainer.get_child_count() != 0:
 		var previous_scene_node = mainContainer.get_child(0)
 		_fade_out(previous_scene_node)
+		yield(_fade_out(previous_scene_node), "completed")
 		previous_scene_node.call_deferred('free')
 	#instanciate the new scene and add it to the tree
 	var sceneToGoInstance = sceneToGo.instance()
@@ -28,15 +29,15 @@ func goto_previousScene():
 	pass
 
 func _fade_out(_container : Node):
-	pass
-#	prints("hola", container)
-#	var transition_rect = ColorRect.new()
-#	var transition_tween = Tween.new()
-#	container.add_child(transition_rect)
-#	container.add_child(transition_tween)
-#
-#	var _fadeIn = transition_tween.interpolate_property(transition_rect, 
-#										"color", Color("00000000"),
-#										Color("000000"),0.5)
-#	var _fadeInStart = transition_tween.start()
-#	yield(transition_tween, "tween_completed")
+	prints("hola", _container)
+	
+	var transition_rect = ColorRect.new()
+	var transition_tween = Tween.new()
+	_container.add_child(transition_rect)
+	_container.add_child(transition_tween)
+
+	var _fadeIn = transition_tween.interpolate_property(transition_rect, 
+										"color", Color("00000000"),
+										Color("000000"),0.5)
+	var _fadeInStart = transition_tween.start()
+	yield(transition_tween, "tween_completed")
