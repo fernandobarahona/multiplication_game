2cdc06ebe21f8452d510e370d052c8ab2d4e78bc
fernandobarahona <33161338+fernandobarahona@users.noreply.github.com>
2022-04-19 21:05:39 -0500

    function names and progress bar

--- scenes_and_scripts/screen__main_menu/main_menu.gd
+++ scenes_and_scripts/screen__main_menu/main_menu.gd
@@ -5,9 +5,9 @@ var _config_menu_scene = load(GlobalConstants.SCENES_AND_SCRIPTS_PATHS["scene__c
 var _login_scene = load(GlobalConstants.SCENES_AND_SCRIPTS_PATHS["scene__login"])
 
 func _ready():
-	var _err1 = get_node("MarginContainer/VBoxContainer/GoToGameBtn").connect("pressed",self,"goToScene",[_game_scene])
-	var _err2 = get_node("MarginContainer/VBoxContainer/GoToSettingsBtn").connect("pressed",self,"goToScene",[_config_menu_scene])
-	var _err3 = get_node("MarginContainer/VBoxContainer/GoToLoginBtn").connect("pressed",self,"goToScene",[_login_scene])
+	var _err1 = get_node("MarginContainer/VBoxContainer/GoToGameBtn").connect("pressed",self,"goto_scene",[_game_scene])
+	var _err2 = get_node("MarginContainer/VBoxContainer/GoToSettingsBtn").connect("pressed",self,"goto_scene",[_config_menu_scene])
+	var _err3 = get_node("MarginContainer/VBoxContainer/GoToLoginBtn").connect("pressed",self,"goto_scene",[_login_scene])
 
-func goToScene(scene):
+func goto_scene(scene):
 	ScreenManager.goto_scene(scene)
