5e00977142794ffeea45604fda243ae78c80f843
fernandobarahona <33161338+fernandobarahona@users.noreply.github.com>
2022-04-19 20:44:31 -0500

    empezar centerContainer

--- scenes_and_scripts/screen__main_menu/main_menu.gd
+++ scenes_and_scripts/screen__main_menu/main_menu.gd
@@ -1,15 +1,13 @@
-extends MarginContainer
-
-class_name MainMenu
+extends CenterContainer
 
 var _game_scene = load(GlobalConstants.SCENES_AND_SCRIPTS_PATHS["scene__game"])
 var _config_menu_scene = load(GlobalConstants.SCENES_AND_SCRIPTS_PATHS["scene__config_menu"])
 var _login_scene = load(GlobalConstants.SCENES_AND_SCRIPTS_PATHS["scene__login"])
 
 func _ready():
-	var _err1 = get_node("VBoxContainer/GoToGameBtn").connect("pressed",self,"goToScene",[_game_scene])
-	var _err2 = get_node("VBoxContainer/GoToSettingsBtn").connect("pressed",self,"goToScene",[_config_menu_scene])
-	var _err3 = get_node("VBoxContainer/GoToLoginBtn").connect("pressed",self,"goToScene",[_login_scene])
+	var _err1 = get_node("MarginContainer/VBoxContainer/GoToGameBtn").connect("pressed",self,"goToScene",[_game_scene])
+	var _err2 = get_node("MarginContainer/VBoxContainer/GoToSettingsBtn").connect("pressed",self,"goToScene",[_config_menu_scene])
+	var _err3 = get_node("MarginContainer/VBoxContainer/GoToLoginBtn").connect("pressed",self,"goToScene",[_login_scene])
 
 func goToScene(scene):
 	ScreenManager.goto_scene(scene)
